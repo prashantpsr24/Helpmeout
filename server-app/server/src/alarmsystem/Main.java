5fd2c5a49250cfbfc7bbd6138e806dcb941ba29d
darkpanther <prashantpsr24@gmail.com>
2015-04-18 16:38:58 +0530

    modified bean xml

---
+++ server-app/server/src/alarmsystem/Main.java
@@ -0,0 +1,17 @@
+package alarmsystem;
+
+	import org.springframework.context.ApplicationContext;
+	import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+	public class Main
+	{
+	   public static void main(String[] args) 
+	   {
+	      ApplicationContext context = 
+	             new ClassPathXmlApplicationContext("Beans.xml");
+
+	      Alarm objA = (Alarm) context.getBean("beanAlarm");
+
+	   }
+	
+	}
