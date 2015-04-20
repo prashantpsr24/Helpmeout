9ed7de9a28049a132128d0566a6d2b822b6c28ce
purva bansal <purva.bansal@gmail.com>
2015-04-20 10:59:10 +0530

    request sent to server

--- HelpMeOut/app/src/main/java/com/example/purva/helpmeout/MainActivity.java
+++ HelpMeOut/app/src/main/java/com/example/purva/helpmeout/MainActivity.java
@@ -21,199 +21,27 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.purva.helpmeout.gpstracking.GPSTracker;
-//public class GPSTracker extends Service implements LocationListener {
-//
-//    private final Context mContext;
-//
-//    // flag for GPS status
-//    boolean isGPSEnabled = false;
-//
-//    // flag for network status
-//    boolean isNetworkEnabled = false;
-//
-//    // flag for GPS status
-//    boolean canGetLocation = false;
-//
-//    Location location; // location
-//    double latitude; // latitude
-//    double longitude; // longitude
-//
-//    // The minimum distance to change Updates in meters
-//    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
-//
-//    // The minimum time between updates in milliseconds
-//    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
-//
-//    // Declaring a Location Manager
-//    protected LocationManager locationManager;
-//
-//    public GPSTracker(Context context) {
-//        this.mContext = context;
-//        getLocation();
-//    }
-//
-//    public Location getLocation() {
-//        try {
-//            locationManager = (LocationManager) mContext
-//                    .getSystemService(LOCATION_SERVICE);
-//
-//            // getting GPS status
-//            isGPSEnabled = locationManager
-//                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
-//
-//            // getting network status
-//            isNetworkEnabled = locationManager
-//                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-//
-//            if (!isGPSEnabled && !isNetworkEnabled) {
-//                // no network provider is enabled
-//            } else {
-//                this.canGetLocation = true;
-//                // First get location from Network Provider
-//                if (isNetworkEnabled) {
-//                    locationManager.requestLocationUpdates(
-//                            LocationManager.NETWORK_PROVIDER,
-//                            MIN_TIME_BW_UPDATES,
-//                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-//                    Log.d("Network", "Network");
-//                    if (locationManager != null) {
-//                        location = locationManager
-//                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-//                        if (location != null) {
-//                            latitude = location.getLatitude();
-//                            longitude = location.getLongitude();
-//                        }
-//                    }
-//                }
-//                // if GPS Enabled get lat/long using GPS Services
-//                if (isGPSEnabled) {
-//                    if (location == null) {
-//                        locationManager.requestLocationUpdates(
-//                                LocationManager.GPS_PROVIDER,
-//                                MIN_TIME_BW_UPDATES,
-//                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-//                        Log.d("GPS Enabled", "GPS Enabled");
-//                        if (locationManager != null) {
-//                            location = locationManager
-//                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
-//                            if (location != null) {
-//                                latitude = location.getLatitude();
-//                                longitude = location.getLongitude();
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        return location;
-//    }
-//
-//    /**
-//     * Stop using GPS listener
-//     * Calling this function will stop using GPS in your app
-//     * */
-//    public void stopUsingGPS(){
-//        if(locationManager != null){
-//            locationManager.removeUpdates(GPSTracker.this);
-//        }
-//    }
-//
-//    /**
-//     * Function to get latitude
-//     * */
-//    public double getLatitude(){
-//        if(location != null){
-//            latitude = location.getLatitude();
-//        }
-//
-//        // return latitude
-//        return latitude;
-//    }
-//
-//    /**
-//     * Function to get longitude
-//     * */
-//    public double getLongitude(){
-//        if(location != null){
-//            longitude = location.getLongitude();
-//        }
-//
-//        // return longitude
-//        return longitude;
-//    }
-//
-//    /**
-//     * Function to check GPS/wifi enabled
-//     * @return boolean
-//     * */
-//    public boolean canGetLocation() {
-//        return this.canGetLocation;
-//    }
-//
-//    /**
-//     * Function to show settings alert dialog
-//     * On pressing Settings button will lauch Settings Options
-//     * */
-//    public void showSettingsAlert(){
-//        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
-//
-//        // Setting Dialog Title
-//        alertDialog.setTitle("GPS is settings");
-//
-//        // Setting Dialog Message
-//        alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
-//
-//        // On pressing Settings button
-//        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
-//            public void onClick(DialogInterface dialog,int which) {
-//                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-//                mContext.startActivity(intent);
-//            }
-//        });
-//
-//        // on pressing cancel button
-//        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-//            public void onClick(DialogInterface dialog, int which) {
-//                dialog.cancel();
-//            }
-//        });
-//
-//        // Showing Alert Message
-//        alertDialog.show();
-//    }
-//
-//    @Override
-//    public void onLocationChanged(Location location) {
-//    }
-//
-//    @Override
-//    public void onProviderDisabled(String provider) {
-//    }
-//
-//    @Override
-//    public void onProviderEnabled(String provider) {
-//    }
-//
-//    @Override
-//    public void onStatusChanged(String provider, int status, Bundle extras) {
-//    }
-//
-//    @Override
-//    public IBinder onBind(Intent arg0) {
-//        return null;
-//    }
-//
-//}
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.text.ParseException;
+
 public class MainActivity extends ActionBarActivity {
 
     private Button b1;
     private LocationManager locationManager;
-    private LocationManager locManager;
-    //private LocationListener locListener = new MyLocationListener();
+
     private GPSTracker gps;
 
     {
@@ -230,7 +58,6 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         b1 = (Button) findViewById(R.id.help_button);
-        locManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         b1.setOnLongClickListener(
                 new Button.OnLongClickListener() {
                     public boolean onLongClick(View v) {
@@ -252,7 +79,40 @@ public class MainActivity extends ActionBarActivity {
                     double longitude = gps.getLongitude();
 
                     Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
-                } else {
+                    JSONObject locationHelp = new JSONObject();
+                    try {
+                        int tempuid=123456;
+                        locationHelp.put("uid",tempuid);
+                        locationHelp.put("type", "help");
+                        locationHelp.put("latitude",latitude);
+                        locationHelp.put("longitude",longitude);
+
+                        HttpClient client = new DefaultHttpClient();
+                        HttpPost post = new HttpPost("https://www.google.com");
+                        post.setHeader("Content-type", "application/json");
+
+                        StringEntity se = new StringEntity(locationHelp.toString());
+                        se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                        post.setEntity(se);
+
+                        HttpResponse response = client.execute(post);
+                        String responseText = EntityUtils.toString(response.getEntity());
+                        JSONObject json = new JSONObject(responseText);
+//                        Log.i("tag", temp);
+                        // can receive output of this here......
+
+                    }
+                    catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    catch (ClientProtocolException e){
+                        e.printStackTrace();
+                    }
+                    catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else {
                     // can't get location
                     // GPS or Network is not enabled
                     // Ask user to enable GPS/network in settings
@@ -264,107 +124,4 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
-
 }
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
-
-
-//    @Override
-//    public void onClick(View v) {
-//        //progress.setVisibility(View.VISIBLE);
-//// exceptions will be thrown if provider is not permitted.
-//        try {
-//            gps_enabled = locManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-//        } catch (Exception ex) {
-//        }
-//        try {
-//            network_enabled = locManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-//        } catch (Exception ex) {
-//        }
-//
-//// don't start listeners if no provider is enabled
-//        if (!gps_enabled || !network_enabled) {
-//            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//            builder.setTitle("Attention!");
-//            builder.setMessage("Sorry, location is not determined. Please enable location providers");
-//            builder.setPositiveButton("OK", this);
-//            builder.setNeutralButton("Cancel", this);
-//            builder.create().show();
-//            //progress.setVisibility(View.GONE);
-//        }
-//
-//        if (gps_enabled) {
-//            locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locListener);
-//        }
-//        if (network_enabled) {
-//            locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locListener);
-//        }
-//    }
-//    class MyLocationListener implements LocationListener {
-//        @Override
-//        public void onLocationChanged(Location location) {
-//            if (location != null) {
-//// This needs to stop getting the location data and save the battery power.
-//                locManager.removeUpdates(locListener);
-//
-//                String longitude = "Londitude: " + location.getLongitude();
-//                String latitude = "Latitude: " + location.getLatitude();
-//                String altitude = "Altitiude: " + location.getAltitude();
-//                String accuracy = "Accuracy: " + location.getAccuracy();
-//                String time = "Time: " + location.getTime();
-//                //send
-//                Toast.makeText(getApplicationContext(), "data received.",
-//                        Toast.LENGTH_SHORT).show();
-//            }
-//        }
-//
-//        @Override
-//        public void onProviderDisabled(String provider) {
-//// TODO Auto-generated method stub
-//
-//        }
-//
-//        @Override
-//        public void onProviderEnabled(String provider) {
-//// TODO Auto-generated method stub
-//
-//        }
-//
-//        @Override
-//        public void onStatusChanged(String provider, int status, Bundle extras) {
-//// TODO Auto-generated method stub
-//
-//        }
-//    }
-//
-//    @Override
-//    public void onClick(DialogInterface dialog, int which) {
-//        if(which == DialogInterface.BUTTON_NEUTRAL){
-//           Toast.makeText(getApplicationContext(), "Sorry, location is not determined. To fix this please enable location providers.",
-//                   Toast.LENGTH_SHORT).show();
-//            //setText("Sorry, location is not determined. To fix this please enable location providers");
-//        }else if (which == DialogInterface.BUTTON_POSITIVE) {
-//            startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-//        }
-//    }
-//}
