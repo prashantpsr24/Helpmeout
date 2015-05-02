1fddaf66cb66caa1bd3d65efebdfefaa665564b8
darkpanther <prashantpsr24@gmail.com>
2015-05-03 02:49:52 +0530

    added function to select nearby users

--- server-app/server/src/alarmsystem/User.java
+++ server-app/server/src/alarmsystem/User.java
@@ -1,7 +1,6 @@
 package alarmsystem;
 
-public class User 
-{
+public class User {
 	private int id;
 	private long uid;
 	private double latitude;
@@ -9,67 +8,64 @@ public class User
 	private int accStatus;
 	private int score;
 	private long victimUid;
-	
-	   public void setLatitude(double latitude)
-	   {
-		   this.latitude=latitude;
-	   }
-	   public void setLongitude(double longitude)
-	   {
-		   this.longitude=longitude;
-	   }
-	   public double getLatitude()
-	   {
-		   return this.latitude;
-	   }
-	   public double getLongitude()
-	   {
-		   return this.longitude;
-	   }
-	   public void setId(int id)
-	   {
-	      this.id  = id;
-	   }
-	   public int getId()
-	   {
-		   return id;
-	   }
-	   public int getAccStatus()
-	   {
-		   return accStatus;
-	   }
-	   public void setAccStatus(int accStatus)
-	   {
-		   this.accStatus=accStatus;
-	   }
-	   
-	   
-	   public int getScore()
-	   {
-		   return score;
-	   }
-	   public void setScore(int score)
-	   {
-		   this.score=score;
-	   }
-	   
-	   
-	   public void setUid(long uid)
-	   {
-	      this.uid  = uid;
-	   }
-	   public long getUid()
-	   {
-		   return uid;
-	   }
 
-	   public void setVictimUid(long victimuid)
-	   {
-	      this.victimUid  = victimuid;
-	   }
-	   public long getVictimUid()
-	   {
-		   return victimUid;
-	   }
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public double getLatitude() {
+		return this.latitude;
+	}
+
+	public double getLongitude() {
+		return this.longitude;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getAccStatus() {
+		return accStatus;
+	}
+
+	public void setAccStatus(int accStatus) {
+		if (this.accStatus == 1 && accStatus == 0)
+			this.accStatus = 1;
+		else
+			this.accStatus = 0;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public void setUid(long uid) {
+		this.uid = uid;
+	}
+
+	public long getUid() {
+		return uid;
+	}
+
+	public void setVictimUid(long victimuid) {
+		this.victimUid = victimuid;
+	}
+
+	public long getVictimUid() {
+		return victimUid;
+	}
 
 }
